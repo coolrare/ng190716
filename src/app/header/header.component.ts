d4c1e1185b488b3b00747ae8af3db05438fe0922
Will <doggy.huang@gmail.com>
2019-07-16 13:40:28 +0800

    04 練習 內嵌繫結、屬性繫結、事件繫結 語法

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  sitename = 'The Will Will Web';
+  subtitle = '記載著 <strong>Will</strong> 在網路世界的學習心得與技術分享';
+  imgurl = '/assets/images/logo.png';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeTitle() {
+    this.sitename += '!';
   }
 
 }
